46e02222639d8c06713a1b020d0d69c0e47af2bd
Zontax <jrvadim04@gmail.com>
2022-10-25 22:41:04 +0300

    Add8

--- Test-1/js/main.js
+++ Test-1/js/main.js
@@ -1,11 +1,13 @@
 // console.log("Текст в консоль");
-// console.info("Info в консоль");
-// console.warn("Попередження!");
-// console.error("Помилка!");
-document.write("Hi!");
+//? console.info("Info в консоль");
+//* console.warn("Попередження!");
+//! console.error("Помилка!");
+
+document.write("<h1>Hello World! </h1>");
 const one = 1;
 var num = 5;
 console.log("Змінна " + num);
 
 const text =
-  "Dolore cillum tempor nulla ea eiusmod excepteur voluptate exercitation est amet nisi adipisicing. Laboris proident nisi anim in irure. Tempor mollit consectetur ullamco non esse sunt deserunt mollit officia incididunt incididunt mollit nisi do. Ea consequat dolor aliquip duis. Enim excepteur exercitation mollit cillum do aute laborum consectetur qui ipsum. Do et reprehenderit nulla ipsum exercitation labore cillum id incididunt commodo ut adipisicing consectetur.";
+  "Dolore cillum tempor nulla ea eiusmod excepteur voluptate exercitation est amet nisi adipisicing. Laboris proident nisi anim in irure. Tempor mollit consectetur ullamco non esse sunt deserunt mollit officia incididunt incididunt mollit nisi do. Ea consequat dolor aliquip duis. Enim excepteur exercitation mollit cillum do aute laborum consectetur qui ipsum. Do et reprehenderit nulla ipsum exercitation labore cillum id incididunt commodo ut adipisicing consectetur. ";
+document.write(text);
