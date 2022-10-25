9fe4584455e6fd75300c9615a50c09de5ca9cccb
Zontax <jrvadim04@gmail.com>
2022-10-25 22:00:57 +0300

    Add6

--- #1/js/main.js
+++ #1/js/main.js
@@ -1,7 +1,14 @@
 document.write("Hi!");
-console.log("Текст в консоль");
-console.info("Info Текст в консоль");
-console.warn("Попередження!");
-console.error("Помилка!");
+// console.log("Текст в консоль");
+// console.info("Info в консоль");
+// console.warn("Попередження!");
+// console.error("Помилка!");
 
-var a=2
+const one = 1;
+var num = 5;
+console.log("Змінна " + num);
+
+const number = false;
+/* if number == false {
+    console.log(number);
+} */
