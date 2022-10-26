ed5a0b2c7f1aaa7ba783c43d174c5abbe147378b
Zontax <jrvadim04@gmail.com>
2022-10-26 10:56:37 +0300

    Add10

---
+++ Test-1/js/test2.js
@@ -0,0 +1,11 @@
+// console.log("Текст в консоль")
+//? console.info("Info в консоль")
+//* console.warn("Попередження!")
+//! console.error("Помилка!")
+
+const pi = Math.PI
+document.write("<h1>" + pi + "</h1>")
+
+for (var i = 100; i > 5; i /= 2) {
+    document.write(i + " ")
+}
